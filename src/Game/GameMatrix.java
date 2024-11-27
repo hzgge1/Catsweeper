93f24ffdc8954b126b1640b5f19e9a233c35f97c
hzgge1 <hzgge1@iscte-iul.pt>
2024-11-27 16:28:02 +0000

    为GameMatrix类里的方法标上注释

--- src/Game/GameMatrix.java
+++ src/Game/GameMatrix.java
@@ -28,7 +28,14 @@ public class GameMatrix {
 	}
 	
 	/**
-	 * 
+	 * 在游戏内创建Game类的默认猫咪数
+	 */
+	private void creatCatInMatrix() {
+		creatCatInMatrix(game.getNumberCats());
+	}
+	
+	/**
+	 * 为游戏矩阵里的每个不为猫咪的位置赋值
 	 */
 	private void putNumberOfCatsInTheNineSquareGrid(){
 		for (int i = 0; i < gameMatrix.length; i++) {
@@ -39,7 +46,13 @@ public class GameMatrix {
 			}
 		}
 	}
-
+	
+	/**
+	 * 检查以矩阵的第i行第j列为中心不超过矩阵的九宫格内的猫咪数
+	 * @param i 矩阵第i行
+	 * @param j 矩阵第j列
+	 * @return 输出Integer类, 以矩阵的第i行第j列为中心不超过矩阵的九宫格内的猫咪数(输出值为0-8)
+	 */
 	private int getNumberOfCatsInTheNineSquareGrid(int i, int j) {
 		int count = 0;
 		for (int k = Math.max(0, i-1); k <= Math.min(i+1, gameMatrix.length-1); k++) {
@@ -49,15 +62,23 @@ public class GameMatrix {
 		}
 		return count;
 	}
-
-	private void creatCatInMatrix() {
-		creatCatInMatrix(game.getNumberCats());
-	}
 	
+	/**
+	 * 输出矩阵的第i行第j列是否为猫咪
+	 * @param i 矩阵第i行
+	 * @param j 矩阵第j列
+	 * @return 输出boolean类, 如果是猫咪输出true, 反则输出false
+	 */
 	public boolean isCat(int i, int j) {
 		return gameMatrix[i][j] == -1;
 	}
 	
+	/**
+	 * 输出矩阵的第i行第j列的值(-1为猫咪, 0为空, 1-8为当前位置为中心不超过矩阵范围的九宫格内猫咪数)
+	 * @param i 矩阵第i行
+	 * @param j 矩阵第j列
+	 * @return 输出Integer类, 矩阵第i行第j列的值
+	 */
 	public int getNumberOfCatsInNineSquareGrid(int i, int j) {
 		return gameMatrix[i][j];
 	}

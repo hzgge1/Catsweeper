aeeaea83a6d85c630795383bf48db98cd61d16b9
hzgge1 <hzgge1@iscte-iul.pt>
2024-11-28 15:23:11 +0000

    为所有的方法添加注释

--- src/GUI/FrameSize.java
+++ src/GUI/FrameSize.java
@@ -14,27 +14,42 @@ public enum FrameSize {
         this.numRows = numRows;
     }
 
-    // 获取窗口宽度(像素)
+    /**
+     * 获取游戏界面宽度(像素)
+     * @return 输出Integer类, 游戏界面宽度
+     */
     public int getWidth() {
         return numCols * buttonSize;
     }
 
-    // 获取窗口高度(像素)
+    /**
+     * 获取游戏界面高度(像素)
+     * @return 输出Integer类, 游戏界面高度
+     */
     public int getHeight() {
         return numRows * buttonSize;
     }
 
-    // 返回网格矩阵
+    /**
+     * 获取游戏界面尺寸获取规定矩阵
+     * @return 输出Integer类矩阵, 游戏矩阵
+     */
     public int[][] getGridMatrix() {
         return new int[numRows][numCols]; // 行数为第一维，列数为第二维
     }
 
-    // 返回按钮列数
+    /**
+     * 获取按钮的列数
+     * @return 输出Integer类, 按钮的列数
+     */
     public int getNumCols() {
         return numCols;
     }
 
-    // 返回按钮行数
+    /**
+     * 获取按钮的行数
+     * @return 输出Integer类, 按钮的行数
+     */
     public int getNumRows() {
         return numRows;
     }
